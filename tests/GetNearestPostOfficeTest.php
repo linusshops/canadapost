baa24b3a1730ac4a1599f61617c251cff3b21a74
Sam Schmidt <samuel@dersam.net>
2015-12-09 14:50:49 -0500

    Define type test.

--- tests/GetNearestPostOfficeTest.php
+++ tests/GetNearestPostOfficeTest.php
@@ -9,5 +9,19 @@
  */
 class GetNearestPostOfficeTest extends PHPUnit_Framework_TestCase
 {
+    public function testLoadByFactory()
+    {
+        $factory = new \LinusShops\CanadaPost\ServiceFactory(
+            "https://ct.soa-gw.canadapost.ca",
+            CP_USER,
+            CP_PASSWORD
+        );
 
+        $service = $factory->getService('GetNearestPostOffice');
+        $this->assertInstanceOf(
+            '\\LinusShops\\CanadaPost\\Services\\GetNearestPostOffice',
+            $service
+        );
+        $this->assertInstanceOf('\LinusShops\CanadaPost\Service', $service);
+    }
 }
