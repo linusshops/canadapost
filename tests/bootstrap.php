12a33c95842ba1f7d7321d527871a1f36ae5cfb4
Sam Schmidt <samuel@dersam.net>
2015-12-09 14:35:29 -0500

    Add canada post dev config handler.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,14 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
+
+$config = json_decode(file_get_contents(__DIR__.'/config.json'), true);
+if ($config == false) {
+    $user = getenv('CP_USER');
+    $pass = getenv('CP_PASSWORD');
+} else {
+    $user = $config['CP_USER'];
+    $pass = $config['CP_PASSWORD'];
+}
+
+define('CP_USER', $user);
+define('CP_PASSWORD', $pass);
